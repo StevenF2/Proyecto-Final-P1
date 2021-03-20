055d06741232d861618de415e79d6d1ffe58f414
Leslie R <lesliern30@hotmail.com>
2021-03-19 20:41:12 -0400

    Insercion de atributos con sus respectivos metodos en clase Cliente y Contrato

--- ProyectoFinal/src/logico/Cliente.java
+++ ProyectoFinal/src/logico/Cliente.java
@@ -1,5 +1,50 @@
 package logico;
 
-public class Cliente {
+import java.util.ArrayList;
 
+public class Cliente {
+	private String cedula;
+	private String nombre;
+	private String direccion;
+	private ArrayList<Contrato> contratos;
+	private static int cantidadCliente = 1;
+	public Cliente(String cedula, String nombre, String direccion) {
+		super();
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.contratos = new ArrayList<Contrato>();
+		Cliente.cantidadCliente++;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public ArrayList<Contrato> getContratos() {
+		return contratos;
+	}
+	public void setContratos(ArrayList<Contrato> contratos) {
+		this.contratos = contratos;
+	}
+	public static int getCantidadCliente() {
+		return cantidadCliente;
+	}
+	public static void setCantidadCliente(int cantidadCliente) {
+		Cliente.cantidadCliente = cantidadCliente;
+	}
+	
 }
--- ProyectoFinal/src/logico/Contrato.java
+++ ProyectoFinal/src/logico/Contrato.java
@@ -1,5 +1,59 @@
 package logico;
 
-public class Contrato {
+import java.util.Calendar;
 
+public class Contrato {
+	private String numeroContrato;
+	private String cedulaCliente;
+	private String nombreProyecto; //se puede cambiar a tipo proyecto, a espera de la siguiente clase
+	private Calendar fechaInicio;
+	private Calendar fechaEntrega;
+	private static int cantidadContrato = 1;
+	public Contrato(String numeroContrato, String cedulaCliente, String nombreProyecto, Calendar fechaInicio,
+			Calendar fechaEntrega) {
+		super();
+		this.numeroContrato = numeroContrato;
+		this.cedulaCliente = cedulaCliente;
+		this.nombreProyecto = nombreProyecto;
+		this.fechaInicio = fechaInicio;
+		this.fechaEntrega = fechaEntrega;
+		Contrato.cantidadContrato++;
+	}
+	public String getNumeroContrato() {
+		return numeroContrato;
+	}
+	public void setNumeroContrato(String numeroContrato) {
+		this.numeroContrato = numeroContrato;
+	}
+	public String getCedulaCliente() {
+		return cedulaCliente;
+	}
+	public void setCedulaCliente(String cedulaCliente) {
+		this.cedulaCliente = cedulaCliente;
+	}
+	public String getNombreProyecto() {
+		return nombreProyecto;
+	}
+	public void setNombreProyecto(String nombreProyecto) {
+		this.nombreProyecto = nombreProyecto;
+	}
+	public Calendar getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Calendar fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Calendar getFechaEntrega() {
+		return fechaEntrega;
+	}
+	public void setFechaEntrega(Calendar fechaEntrega) {
+		this.fechaEntrega = fechaEntrega;
+	}
+	public static int getCantidadContrato() {
+		return cantidadContrato;
+	}
+	public static void setCantidadContrato(int cantidadContrato) {
+		Contrato.cantidadContrato = cantidadContrato;
+	}
+	
 }
