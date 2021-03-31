7e24537ebdcfebffaaad925c5b8a46a01c6abc2b
Leslie R <lesliern30@hotmail.com>
2021-03-31 19:39:37 -0400

    Visual: agregando clase Login

---
+++ ProyectoFinal/src/Visual/Login.java
@@ -0,0 +1,90 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+
+public class Login extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
+	private JButton btnIngresar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			Login dialog = new Login();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Login() {
+		setBounds(100, 100, 450, 300);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(new Color(255, 255, 255));
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(51, 102, 153), new Color(51, 102, 153)));
+			panel.setBackground(new Color(255, 255, 255));
+			contentPanel.add(panel, BorderLayout.CENTER);
+			panel.setLayout(null);
+			{
+				JLabel lblNewLabel = new JLabel("Usuario");
+				lblNewLabel.setBounds(93, 75, 46, 14);
+				panel.add(lblNewLabel);
+			}
+			{
+				JLabel lblNewLabel_1 = new JLabel("Contrase\u00F1a");
+				lblNewLabel_1.setBounds(93, 126, 104, 14);
+				panel.add(lblNewLabel_1);
+			}
+			{
+				textField = new JTextField();
+				textField.setBounds(170, 72, 169, 20);
+				panel.add(textField);
+				textField.setColumns(10);
+			}
+			{
+				textField_1 = new JTextField();
+				textField_1.setBounds(170, 123, 169, 20);
+				panel.add(textField_1);
+				textField_1.setColumns(10);
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setBackground(new Color(51, 102, 153));
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				btnIngresar = new JButton("Ingresar");
+				btnIngresar.setBorderPainted(false);
+				btnIngresar.setBackground(new Color(0, 51, 102));
+				btnIngresar.setForeground(new Color(255, 255, 255));
+				btnIngresar.setActionCommand("Cancel");
+				buttonPane.add(btnIngresar);
+			}
+		}
+	}
+}
