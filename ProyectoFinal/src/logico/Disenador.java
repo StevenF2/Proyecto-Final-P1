32d80961feb544e5b3bd2b326bb72f2e1cda21b6
Leslie R <lesliern30@hotmail.com>
2021-04-01 08:14:25 -0400

    Modificacion: agregando atributo de experiencia

--- ProyectoFinal/src/logico/Disenador.java
+++ ProyectoFinal/src/logico/Disenador.java
@@ -1,11 +1,20 @@
 package logico;
 
 public class Disenador extends Empleado {
-
+	
+	private int experiencia;
 	public Disenador(String cedula, String nombre, String apellido, String direccion,String telefono, String sexo, float salario,
 			String nombreProyecto, String evaluacion, float precioHora) {
 		super(cedula, nombre, apellido, direccion,telefono, sexo, salario, nombreProyecto, evaluacion, precioHora);
 		
+	}
+	
+	public int getExperiencia() {
+		return experiencia;
+	}
+
+	public void setExperiencia(int experiencia) {
+		this.experiencia = experiencia;
 	}
 
 	@Override
