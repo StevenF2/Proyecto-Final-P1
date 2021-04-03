8d6006862e783d9657f25afd90bfb44dbbef8a63
Leslie R <lesliern30@hotmail.com>
2021-04-03 10:07:56 -0400

    Agregar parte visual y filtro por tipo de empleado

--- ProyectoFinal/src/Visual/MostrarEmpleado.java
+++ ProyectoFinal/src/Visual/MostrarEmpleado.java
@@ -2,6 +2,7 @@ package Visual;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -10,12 +11,24 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
 
+import logico.Disenador;
 import logico.Empresa;
+import logico.Jefe;
+import logico.Planificador;
+import logico.Programador;
+import logico.Secretario;
 
 import java.awt.Color;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MostrarEmpleado extends JDialog {
 
@@ -24,6 +37,7 @@ public class MostrarEmpleado extends JDialog {
 	private static DefaultTableModel model;
 	private static Object[] rows;
 	private JButton btnCancelar;
+	private JComboBox cmbEmpleados;
 
 	/**
 	 * Launch the application.
@@ -42,21 +56,23 @@ public class MostrarEmpleado extends JDialog {
 	 * Create the dialog.
 	 */
 	public MostrarEmpleado() {
-		setBounds(100, 100, 590, 435);
+		setBounds(100, 100, 590, 467);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(new Color(255, 255, 255));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new BorderLayout(0, 0));
+		contentPanel.setLayout(null);
 		{
 			JPanel panel = new JPanel();
+			panel.setBounds(5, 49, 564, 341);
 			panel.setBackground(new Color(255, 255, 255));
-			contentPanel.add(panel, BorderLayout.CENTER);
-			panel.setLayout(new BorderLayout(0, 0));
+			contentPanel.add(panel);
+			panel.setLayout(null);
 			{
 				JScrollPane scrollPane = new JScrollPane();
-				panel.add(scrollPane, BorderLayout.CENTER);
+				scrollPane.setBounds(0, 0, 564, 385);
+				panel.add(scrollPane);
 				{
 					tblEmpleado = new JTable();
 					tblEmpleado.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -64,17 +80,25 @@ public class MostrarEmpleado extends JDialog {
 					String[] encabezados = {"Cedula", "Nombre", "Apellido", "Pago por Hora", "Clasificacion"};
 					model.setColumnIdentifiers(encabezados);
 					tblEmpleado.setModel(model);
-					tblEmpleado.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 					tblEmpleado.getTableHeader().setReorderingAllowed(false);
-					TableColumnModel columM = tblEmpleado.getColumnModel();
-					columM.getColumn(1).setPreferredWidth(150);
-					columM.getColumn(2).setPreferredWidth(150);
-					columM.getColumn(4).setPreferredWidth(150);
-
 					scrollPane.setViewportView(tblEmpleado);
 				}
 			}
 		}
+		
+		cmbEmpleados = new JComboBox();
+		cmbEmpleados.setModel(new DefaultComboBoxModel(new String[] {"Todo", "Jefe", "Disenador", "Programador", "Planificador", "Secretario"}));
+		cmbEmpleados.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				cargarEmpleados(cmbEmpleados.getSelectedItem().toString());
+			}
+		});
+		cmbEmpleados.setBounds(88, 21, 132, 20);
+		contentPanel.add(cmbEmpleados);
+		
+		JLabel lblNewLabel = new JLabel("Filtrar por");
+		lblNewLabel.setBounds(10, 24, 100, 14);
+		contentPanel.add(lblNewLabel);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setBackground(new Color(51, 102, 153));
@@ -82,6 +106,11 @@ public class MostrarEmpleado extends JDialog {
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				btnCancelar = new JButton("Cancelar");
+				btnCancelar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						dispose();
+					}
+				});
 				btnCancelar.setBorderPainted(false);
 				btnCancelar.setBackground(new Color(51, 102, 153));
 				btnCancelar.setForeground(new Color(255, 255, 255));
@@ -89,19 +118,54 @@ public class MostrarEmpleado extends JDialog {
 				buttonPane.add(btnCancelar);
 			}
 		}
-		cargarEmpleados();
+		cargarEmpleados(cmbEmpleados.getSelectedItem().toString());
 	}
 	//remember to change it to public and static
-	public static void cargarEmpleados() {
-		rows = new Object[model.getColumnCount()];
+	public static void cargarEmpleados(String selectedItem) {
+		//rows = new Object[model.getColumnCount()];
 		model.setRowCount(0);
 		for(int i = 0; i < Empresa.getInstance().getEmpleados().size(); i++) {
-			rows[0] = Empresa.getInstance().getEmpleados().get(i).getCedula();
+			if(selectedItem.equalsIgnoreCase("Jefe")) {
+				if(Empresa.getInstance().getEmpleados().get(i) instanceof Jefe) {
+					model.addRow(insertInRow(i));
+				}
+			} else if (selectedItem.equalsIgnoreCase("Programador")) {
+				if(Empresa.getInstance().getEmpleados().get(i) instanceof Programador) {
+					model.addRow(insertInRow(i));
+				}
+			}else if (selectedItem.equalsIgnoreCase("Disenador")) {
+				if(Empresa.getInstance().getEmpleados().get(i) instanceof Disenador) {
+					model.addRow(insertInRow(i));
+				}
+			} else if (selectedItem.equalsIgnoreCase("Planificador")) {
+				if(Empresa.getInstance().getEmpleados().get(i) instanceof Planificador) {
+					model.addRow(insertInRow(i));
+				}
+			}else if (selectedItem.equalsIgnoreCase("Secretario")) {
+				if(Empresa.getInstance().getEmpleados().get(i) instanceof Secretario) {
+					model.addRow(insertInRow(i));
+				}
+			}else if (selectedItem.equalsIgnoreCase("Todo")) {
+				model.addRow(insertInRow(i));
+			}
+			/*rows[0] = Empresa.getInstance().getEmpleados().get(i).getCedula();
 			rows[1] = Empresa.getInstance().getEmpleados().get(i).getNombre();
 			rows[2] = Empresa.getInstance().getEmpleados().get(i).getApellido();
 			rows[3] = Empresa.getInstance().getEmpleados().get(i).getSalario();
-			rows[4] = Empresa.getInstance().getEmpleados().get(i).getClass().getSimpleName();
-			model.addRow(rows);
+			rows[4] = Empresa.getInstance().getEmpleados().get(i).getClass().getSimpleName();*/
+			//model.addRow(rows);
 		}
+	}
+	
+	private static Object[] insertInRow(int index) {
+		rows = new Object[model.getColumnCount()];
+		rows[0] = Empresa.getInstance().getEmpleados().get(index).getCedula();
+		rows[1] = Empresa.getInstance().getEmpleados().get(index).getNombre();
+		rows[2] = Empresa.getInstance().getEmpleados().get(index).getApellido();
+		rows[3] = Empresa.getInstance().getEmpleados().get(index).getSalario();
+		rows[4] = Empresa.getInstance().getEmpleados().get(index).getClass().getSimpleName();
+		
+		return rows;
+		
 	}
 }
