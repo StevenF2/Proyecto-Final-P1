634c456ce1315db833305cd95fb7ee9e318912f4
Leslie R <lesliern30@hotmail.com>
2021-04-05 22:39:50 -0400

    Agregando funcionalidad a tabla de mostrar contrato

--- ProyectoFinal/src/Visual/MostrarContrato.java
+++ ProyectoFinal/src/Visual/MostrarContrato.java
@@ -9,21 +9,34 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+
+import logico.Empresa;
+
 import javax.swing.border.EtchedBorder;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JTable;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MostrarContrato extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	private JButton btnCancelar;
 	private JTable tblMostrarProyectos;
+	private static DefaultTableModel model;
+	private static Object[] rows;
+
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		try {
 			MostrarContrato dialog = new MostrarContrato();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
@@ -31,13 +44,17 @@ public class MostrarContrato extends JDialog {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
+	}*/
 
 	/**
 	 * Create the dialog.
 	 */
 	public MostrarContrato() {
-		setBounds(100, 100, 548, 368);
+		setBounds(100, 100, 723, 422);
+		setLocationRelativeTo(null);
+		String columns[] = {"Num. Contrato", "Cedula Cliente", "Nombre Cliente", "Fecha Inicio", "Fecha Entrega", "Monto Total"};
+		model = new DefaultTableModel();
+		model.setColumnIdentifiers(columns);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(new Color(255, 255, 255));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -55,6 +72,9 @@ public class MostrarContrato extends JDialog {
 				panel.add(scrollPane, BorderLayout.CENTER);
 				{
 					tblMostrarProyectos = new JTable();
+					tblMostrarProyectos.setModel(model);
+					tblMostrarProyectos.setBackground(new Color(255, 255, 255));
+					tblMostrarProyectos.getTableHeader().setReorderingAllowed(false);
 					scrollPane.setViewportView(tblMostrarProyectos);
 				}
 			}
@@ -66,6 +86,11 @@ public class MostrarContrato extends JDialog {
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				btnCancelar = new JButton("Cancelar");
+				btnCancelar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						dispose();
+					}
+				});
 				btnCancelar.setBorderPainted(false);
 				btnCancelar.setForeground(new Color(255, 255, 255));
 				btnCancelar.setBackground(new Color(51, 102, 153));
@@ -73,6 +98,25 @@ public class MostrarContrato extends JDialog {
 				buttonPane.add(btnCancelar);
 			}
 		}
+		cargarContratos();
+	}
+	private static void cargarContratos() {
+		model.setRowCount(0);
+		rows = new Object[model.getColumnCount()];
+		DateFormat dtf = new SimpleDateFormat("dd MM yyyy");
+		String inicio ,finalFecha = "";
+		for(int i = 0; i < Empresa.getInstance().getContratos().size(); i++) {
+			rows[0] = Empresa.getInstance().getContratos().get(i).getNumeroContrato();
+			rows[1] = Empresa.getInstance().getContratos().get(i).getCedulaCliente();
+			rows[2] = Empresa.getInstance().getContratos().get(i).getCliente().getNombre();
+			inicio = dtf.format(Empresa.getInstance().getContratos().get(i).getProyecto().getFechaInicio());
+			finalFecha = dtf.format(Empresa.getInstance().getContratos().get(i).getProyecto().getFechaEntrega());
+			rows[3] = inicio;
+			rows[4] = finalFecha;
+			rows[5] = Empresa.getInstance().getContratos().get(i).getMontoTotal();
+			model.addRow(rows);
+		}
+		
 	}
 
 }
