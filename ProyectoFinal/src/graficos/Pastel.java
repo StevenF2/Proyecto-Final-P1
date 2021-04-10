22e0cd2e4fce720c16ce6d42f3bf53e44bc809da
Steven <usuario@usuario-PC>
2021-04-10 14:46:28 -0800

    Se agrego la clase Pastel

---
+++ ProyectoFinal/src/graficos/Pastel.java
@@ -0,0 +1,47 @@
+package graficos;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+public class Pastel extends JFrame{
+    JPanel panel;
+    public Pastel(){
+        setTitle("Como Hacer Graficos con Java");
+        setSize(800,600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        init();
+    }
+ 
+    private void init() {
+        panel = new JPanel();
+        getContentPane().add(panel);
+        // Fuente de Datos
+        DefaultPieDataset data = new DefaultPieDataset();
+        data.setValue("C", 40);
+        data.setValue("Java", 45);
+        data.setValue("Python", 15);
+ 
+        // Creando el Grafico
+        JFreeChart chart = ChartFactory.createPieChart(
+         "Ejemplo Rapido de Grafico en un ChartFrame", 
+         data, 
+         true, 
+         true, 
+         false);
+ 
+        // Crear el Panel del Grafico con ChartPanel
+        ChartPanel chartPanel = new ChartPanel(chart);
+        panel.add(chartPanel);
+    }
+    
+    public static void main(String args[]){
+        new Pastel().setVisible(true);
+    }
+}
